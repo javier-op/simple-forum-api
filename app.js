8ad59cc1b94cda66854b1683a3af9b35a0c0564e
Javier Morales <javiermoralesr95@gmail.com>
2021-10-12 21:15:17 -0300

    Added User model and implemented register endpoint.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 require("dotenv").config()
 require("./config/database").connect()
 
@@ -6,5 +8,50 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+const User = require("./model/user")
+
+app.post("/register", async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        if (!(username && email && password)) {
+            req.statusCode(400).send("All input is required.");
+        }
+
+        const usernameMatch = await User.findOne({ username });
+        const emailMatch = await User.findOne({ email });
+        console.log(usernameMatch);
+        if (usernameMatch) {
+            return res.status(409).send("Username already exists. Please login.");
+        } else if (emailMatch) {
+            return res.status(409).send("Email already in use. Please use another.");
+        }
+
+        encryptedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+        });
+
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            { expiresIn: "2h" }
+        );
+
+        user.token = token;
+
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.post("/login", (req, res) => {
+
+});
 
 module.exports = app;
