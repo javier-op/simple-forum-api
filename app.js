60367a19329234fe6b2263a69f9054e501d83c70
Javier Morales <javiermoralesr95@gmail.com>
2021-10-13 18:56:26 -0300

    Added authentication middleware and test endpoint.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { connect } from "./config/database.js";
 import express from "express";
 import { userLogin, userRegister } from "./controllers/userController.js"
+import { verifyToken as auth } from "./middleware/auth.js";
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,8 @@ connect();
 
 app.post("/login", userLogin);
 app.post("/register", userRegister);
+app.get("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome 🎉")
+});
 
 export { app };
