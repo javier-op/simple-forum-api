702dc1cb750ec1651d806d3d38aa39682135f236
Javier Morales <javiermoralesr95@gmail.com>
2021-10-14 14:56:32 -0300

    Updated models.

--- models/comment.js
+++ models/comment.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-    author: {type: mongoose.ObjectId},
-    thread: {type: mongoose.ObjectId},
+    author: {
+        user_id: {type: mongoose.ObjectId},
+        username: {type: String},
+        email: {type: String}
+    },
+    thread_id: {type: mongoose.ObjectId},
     createdAt: {type: Date},
     updatedAt: {type: Date},
     content: {type: String},
-    deleted: {type: Boolean}
+    deleted: {type: Boolean, select: false, default: false}
 });
 
 const Comment = mongoose.model("comment", commentSchema);
--- models/thread.js
+++ models/thread.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 const threadSchema = new mongoose.Schema({
-    author: {type: mongoose.ObjectId},
+    author: {
+        user_id: {type: mongoose.ObjectId},
+        username: {type: String},
+        email: {type: String}
+    },
     createdAt: {type: Date},
     updatedAt: {type: Date},
     title: {type: String},
     content: {type: String},
-    deleted: {type: Boolean}
+    deleted: {type: Boolean, select: false, default: false}
 });
 
 const Thread = mongoose.model("thread", threadSchema);
